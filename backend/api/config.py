73a87c146663258a6efd87c1410f354327d584bd
Zachary Laney <theeliteoneshot@gmail.com>
2022-04-02 21:28:46 -0500

    Additional work
    Added docker configuration for backend and frontend

--- backend/api/config.py
+++ backend/api/config.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
-
 from pydantic import BaseSettings
+import os
 
 
 class Settings(BaseSettings):
@@ -19,18 +19,14 @@ class Settings(BaseSettings):
     NONSENSE_API_PREFIX = "/v1/nonsense"
     STUFF_API_PREFIX= "/v1/stuff"
 
-    BACKEND_HOST = "localhost"
-    BACKEND_PORT = 5000
+    DATABASE_HOST = os.getenv('DATABASE_HOST')
+    DATABASE_PORT = os.getenv('DATABASE_PORT')
 
     API_TILE = "Example API"
     API_VERSION = 1.0
 
     NONSENSE_API_PREFIX = "/v1/nonsense"
     STUFF_API_PREFIX= "/v1/stuff"
-
-    # Frontend Server
-    FRONTEND_HOST = "127.0.0.1"
-    FRONTEND_PORT = 5001
 
     # CORS
     CORS_ORIGINS = [
